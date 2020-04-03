ae45e6e0b90fecd54e8df704e00a61f3d4b31189
Yodhan1 <61077396+Yodhan1@users.noreply.github.com>
2020-04-03 13:34:51 +0700

    Update soal4c.c

--- soal4/soal4c.c
+++ soal4/soal4c.c
@@ -1,1 +1,35 @@
-soal4c
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<string.h> 
+#include<sys/wait.h> 
+
+int main() 
+{ 
+	pid_t pid;
+    int fd[2];
+    int status;
+    if (pipe(fd)==-1) 
+	{ 
+		fprintf(stderr, "Pipe Failed" ); 
+		return 1; 
+	} 
+    pid = fork();
+
+    if (pic < 0 ){
+        fprintf(stderr, "fork Failed" ); 
+		return 1;
+    }else if(pid == 0){
+        close(1);
+        dup2(fd[1]);
+        close(fd[0]);
+        execlp("ls","ls",NULL);
+    }else{
+        wait(&status);
+        close(0);
+        dup(fd[0]);
+        close(fd[1]);
+        execlp("wc","wc","-l",NULL);
+    }
+} 
